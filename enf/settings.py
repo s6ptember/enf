d8e899953cb832d46d21d70e34175bd4fa2e8410
september <september@death.com>
2025-06-28 19:01:47 +0300

    добавили приложение users(forms, views, urls, models)

--- enf/settings.py
+++ enf/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     'main',
     'cart',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -143,3 +144,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_COOKIE_AGE = 86400 # 30 дней
 SESSION_SAVE_EVERY_REQUEST = True
+
+AUTH_USER_MODEL = 'users.CustomUser'
